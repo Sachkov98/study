1512869a5eccec46923fc290a65a4c7c6c38edef
Sachkov98 <sachkov98@gmail.com>
2023-12-03 21:22:43 +0300

    new 03.12.2023

--- app.go
+++ app.go
@@ -11,68 +11,85 @@ import (
 	"time"
 )
 
-func main() {
-	rest_indus()
+type orders struct {
+	Content []struct {
+		OrderId     int64  `json:"order_id"`
+		Status      string `json:"status"`
+		StoreId     int64  `json:"store_id"`
+		DateCreated string `json:"date_created"`
+	} `json:"content"`
 }
 
-// add
-func rest_indus() {
+type connectionString struct {
+	user, password, dbname, sslmode string
+}
 
-	type T struct {
-		Content []struct {
-			OrderId     int64  `json:"order_id"`
-			Status      string `json:"status"`
-			StoreId     int64  `json:"store_id"`
-			DateCreated string `json:"date_created"`
-		} `json:"content"`
+func main() {
+
+	for {
+
+		var (
+			orders1           orders
+			connectionString1 = connectionString{
+				user:     "myUser",
+				password: "myPassword",
+				dbname:   "postgres",
+				sslmode:  "disable"}
+		)
+
+		resp, err := http.Get("http://localhost:8081")
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = json.Unmarshal(body, &orders1)
+
+		if err != nil {
+
+			log.Fatal(err)
+		}
+
+		//connectionString := "user=myUser password=myPassword dbname=postgres sslmode=disable"
+		db, err := sql.Open("postgres", fmt.Sprintf("user=%s password=%s dbname=%s sslmode=%s",
+			connectionString1.user,
+			connectionString1.password,
+			connectionString1.dbname,
+			connectionString1.sslmode))
+
+		if err != nil {
+			panic(err)
+		}
+
+		err = db.Ping()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Успешное подключение к БД")
+		defer db.Close()
+
+		sqlStatement := `INSERT INTO list (OrderId, Status, StoreId, DateCreated) VALUES ($1, $2, $3, $4)`
+		_, err = db.Exec(sqlStatement,
+			orders1.Content[0].OrderId,
+			orders1.Content[0].Status,
+			orders1.Content[0].StoreId,
+			orders1.Content[0].DateCreated)
+
+		if err != nil {
+			panic(err)
+		} else {
+			fmt.Println("\nRow inserted successfully!")
+		}
+		time.Sleep(60 * time.Second)
+
 	}
-
-	var t1 T
-
-	resp, err := http.Get("http://localhost:8081")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = json.Unmarshal(body, &t1)
-
-	if err != nil {
-
-		log.Fatal(err)
-	}
-	fmt.Println(t1)
-	fmt.Println(t1.Content)
-
-	connStr := "user=myUser password=myPassword dbname=postgres sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		panic(err)
-	}
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Успешное подключение к БД")
-	defer db.Close()
-
-	sqlStatement := `INSERT INTO list (OrderId, Status, StoreId, DateCreated) VALUES ($1, $2, $3, $4)`
-	_, err = db.Exec(sqlStatement, t1.Content[0].OrderId, t1.Content[0].Status, t1.Content[0].StoreId, t1.Content[0].DateCreated)
-	if err != nil {
-		panic(err)
-	} else {
-		fmt.Println("\nRow inserted successfully!")
-	}
-	time.Sleep(60 * time.Second)
-
 }
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module awesomeProject
+module study
 
 go 1.21
 
